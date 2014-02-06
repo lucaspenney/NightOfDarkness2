9b36d2f166a9c12b8c6cb1ae2ca1d149ed935b6f
Lucas Penney <lucaspenney@gmail.com>
2014-02-05 20:50:30 -0800

    Added release build grunt task
    Uglifying JS and copying all neccesary files to build directory.

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,10 +14,40 @@ module.exports = function(grunt) {
 				src: 'js/*',
 				dest: 'compiled.js'
 			}
+		},
+		uglify: {
+			options: {
+				mangle: false
+			},
+			build: {
+				src: 'js/*.js',
+				dest: 'build/compiled.js'
+			}
+		},
+		copy: {
+			build: {
+				files: [{
+					src: 'images/*',
+					dest: 'build/images/'
+				}, {
+					src: 'sounds/*',
+					dest: 'build/sounds/'
+				}, {
+					src: 'maps/*',
+					dest: 'build/maps/'
+				}, {
+					src: 'game.html',
+					dest: 'build/'
+				}]
+			}
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+
 	grunt.registerTask('default', ['concat']);
+	grunt.registerTask('build', ['copy:build', 'uglify:build']);
 };
